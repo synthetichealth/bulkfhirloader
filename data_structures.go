e9369bbf3f8fa772850bd92a84592bf0afce826d
mulcahyk <kmulcahy@mitre.org>
2016-09-22 17:54:33 -0400

    Changes to accommodate new schema

--- data_structures.go
+++ data_structures.go
@@ -1,17 +1,22 @@
 package bulkfhirloader
 
 type RawStats struct {
-	ID              string          `bson:"_id" json:"_id"`
-	Location        PgFips          `bson:"location,omitempty" json:"location,omitempty"`
-	Gender          string          `bson:"gender,omitempty" json:"gender"`
-	DeceasedBoolean bool            `bson:"deceasedboolean,omitempty" json:"deceasedboolean,omitempty"`
-	Conditions      []ConditionCode `bson:"conditions" json:"conditions"`
+	ID               string          `bson:"_id" json:"_id"`
+	Location         PgFips          `bson:"location,omitempty" json:"location,omitempty"`
+	Gender           string          `bson:"gender,omitempty" json:"gender"`
+	AgeRange         int             `bson:"agerange" json:"agerange"`
+	Age              int             `bson:"age" json:"age"`
+	DeceasedBoolean  bool            `bson:"deceasedboolean,omitempty" json:"deceasedboolean,omitempty"`
+	Conditions       []ConditionCode `bson:"conditions,omitempty" json:"conditions,omitempty"`
+	UniqueConditions []int           `bson:"uniqueconditions,omitempty" json:"uniqueconditions,omitempty"`
+	UniqueDiseases   []int           `bson:"uniquediseases,omitempty" json:"uniquediseases,omitempty"`
 }
 
 type ConditionCode struct {
-	System    string `bson:"system,omitempty" json:"system,omitempty"`
-	Code      string `bson:"code,omitempty" json:"code,omitempty"`
-	DiseaseFP int32  `bson:"diseasefp" json:"diseasefp"`
+	System      string `bson:"system,omitempty" json:"system,omitempty"`
+	Code        string `bson:"code,omitempty" json:"code,omitempty"`
+	ConditionID int    `bson:"conditionid" json:"conditionid"`
+	DiseaseID   int    `bson:"diseaseid" json:"diseaseid"`
 }
 
 type PgFips struct {
@@ -24,4 +29,9 @@ type PgFips struct {
 type DiseaseKey struct {
 	CodeSystem  string `bson:"codesystem,omitempty"`
 	CodeSysCode string `bson:"codesyscode,omitempty"`
+}
+
+type DiseaseGroup struct {
+	ConditionID int `bson:"conditionid" json:"conditionid"`
+	DiseaseID   int `bson:"diseaseid" json:"diseaseid"`
 }
